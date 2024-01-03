3e337d6642c247120186d241ef3bc6057d087274
Arsalan Ajmal <ars4arsalan@gmail.com>
2024-01-03 12:37:00 +0500

    Fixed the Register block rendering issue

--- Know/Module/Controller/Index/Index.php
+++ Know/Module/Controller/Index/Index.php
@@ -12,18 +12,45 @@ class Index implements ActionInterface
 
     private JsonFactory $jsonFactory;
 
-    public function __construct(PageFactory $pageFactory, JsonFactory $jsonFactory)
-    {
+    /**
+     * @var \Magento\Customer\Block\DataProviders\AddressAttributeData
+     */
+    private $attributeData;
+
+    /**
+     * @var \Magento\Customer\ViewModel\Address\RegionProvider
+     */
+    private $regionProvider;
+
+    /**
+     * @var \Magento\Customer\ViewModel\CreateAccountButton
+     */
+    private $createAccountButtonViewModel;
+
+    public function __construct(
+        PageFactory $pageFactory,
+        JsonFactory $jsonFactory,
+        \Magento\Customer\Block\DataProviders\AddressAttributeData $addressAttributeData,
+        \Magento\Customer\ViewModel\Address\RegionProvider $regionProvider,
+        \Magento\Customer\ViewModel\CreateAccountButton $createAccountButtonViewModel
+    ) {
         $this->pageFactory = $pageFactory;
         $this->jsonFactory = $jsonFactory;
+        $this->attributeData = $addressAttributeData;
+        $this->regionProvider = $regionProvider;
+        $this->createAccountButtonViewModel = $createAccountButtonViewModel;
     }
 
     public function execute()
     {
         $resultPage = $this->pageFactory->create();
-        $html = $resultPage->getLayout()
-            ->createBlock(\Know\Module\Block\Form\Register::class)
-            ->setTemplate('Magento_Customer::form/register.phtml')
+        /** @var \Know\Module\Block\Form\Register $block */
+        $block = $resultPage->getLayout()
+            ->createBlock(\Know\Module\Block\Form\Register::class);
+        $block->setData('attribute_data' , $this->attributeData);
+        $block->setData('region_provider' , $this->regionProvider);
+        $block->setData('create_account_button_view_model' , $this->createAccountButtonViewModel);
+        $html = $block->setTemplate('Magento_Customer::form/register.phtml')
             ->toHtml();
 
         $resultJson = $this->jsonFactory->create();
